e7ff38c07f42198c57c03ef8f94175dcd576c5c4
rioaldie <rioaldierwanto@gmail.com>
2023-09-08 18:08:04 +0700

    websocket

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,8 @@ import '../styles/responsive.scss';
 
 import App from './views/app';
 import swRegister from './utils/sw-register';
+import WebSocketInitiator from './utils/websocket-initiator';
+import CONFIG from './global/config';
 
 const app = new App({
   button: document.querySelector('#btn-menu'),
@@ -16,4 +18,5 @@ window.addEventListener('hashchange', () => {
 window.addEventListener('load', () => {
   app.renderPage();
   swRegister();
+  WebSocketInitiator.init(CONFIG.WEB_SOCKET_SERVER);
 });
